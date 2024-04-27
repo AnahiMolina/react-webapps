45fdd537aeaa77ccb597cc70b7d7d0ab73e0b457
AnahiMolina <anahi_molina11@outlook.com>
2024-04-26 18:18:10 -0600

    Agregar funcionalidades de Calculadora

--- src/Pages/Calculadora.jsx
+++ src/Pages/Calculadora.jsx
@@ -1,69 +1,74 @@
-import React from 'react'
-import Contenedor from '../Layouts/Contenedor';
+import React, { useRef } from 'react'
+import Contenedor from '../Layouts/Contenedor'
 
 export default function Calculadora() {
-  let operacion = "";
-  let operadorActual = "";
-  let operadorAnterior = "";
-  let resultado = 0;
+  const displayRef = useRef(null)
+  let operacion = ""
 
-  function agregarNum(numero) {
-    operacion += numero;
-    actualizarDisplay();
+  function agregarCaracter(caracter) {
+    if (caracter === '.' && operacion.split(/[+\-*\/]/).pop().includes('.')) {
+      return
+    }
+    operacion += caracter
+    actualizarDisplay()
   }
-  function agregarOperador(operador) {
-    operadorAnterior = operadorActual;
-    operadorActual = operador;
-    operacion += operador;
-    actualizarDisplay();
-  }
+
   function limpiar() {
-    operacion = "";
-    operadorActual = "";
-    operadorAnterior = "";
-    resultado = 0;
-    actualizarDisplay();
+    operacion = ""
+    actualizarDisplay()
   }
-  function calculo() {
-    resultado = eval(operacion);
+
+  function calcular() {
+    const resultado = eval(operacion);
     operacion = resultado.toString();
-    operadorActual = "";
-    operadorAnterior = "";
     actualizarDisplay();
   }
-  function actualizarDisplay() {
-    document.getElementById("display").value = operacion;
+
+  function calcularRaiz() {
+    const resultado = Math.sqrt(eval(operacion));
+    operacion = resultado.toString();
+    actualizarDisplay();
   }
+
+  function actualizarDisplay() {
+    displayRef.current.value = operacion;
+  }
+
   return (
     <>
       <Contenedor titulo='Calculadora' icono='bi bi-calculator' clase='calculadora'>
         <div className='row d-flex justify-content-center'>
-          <div className='col-sm-5'>
+          <div className='col-md-4'>
             <div className='card p-4'>
-              <input type="text" className="input form-control input_seguridad mb-4" id="display" disabled />
-              <div className='col-12 d-flex justify-content-between'>
-                <button onClick={() => agregarNum(7)} className="btn btn-numero">7</button>
-                <button onClick={() => agregarNum(8)} className="btn btn-numero">8</button>
-                <button onClick={() => agregarNum(9)} className="btn btn-numero">9</button>
-                <button onClick={() => agregarOperador('/')} className="btn btn-tecla" >÷</button>
+              <input type="text" className="input form-control input_seguridad mb-4" ref={displayRef} disabled />
+              <div className='col-md-12 d-flex justify-content-between mb-1'>
+                <button onClick={() => limpiar()} className="btn btn-tecla" >CE</button>
+                <button onClick={() => calcularRaiz()} className="btn btn-tecla" >√</button>
+                <button onClick={() => agregarCaracter('**')} className="btn btn-tecla" >^</button>
+                <button onClick={() => agregarCaracter('+')} className="btn btn-tecla" >+</button>
               </div>
-              <div className='col-12 d-flex justify-content-between'>
-                <button onClick={() => agregarNum(4)} className="btn btn-numero">4</button>
-                <button onClick={() => agregarNum(5)} className="btn btn-numero">5</button>
-                <button onClick={() => agregarNum(6)} className="btn btn-numero">6</button>
-                <button onClick={() => agregarOperador('*')} className="btn btn-tecla" >×</button>
+              <div className='col-md-12 d-flex justify-content-between mb-1'>
+                <button onClick={() => agregarCaracter('7')} className="btn btn-numero">7</button>
+                <button onClick={() => agregarCaracter('8')} className="btn btn-numero">8</button>
+                <button onClick={() => agregarCaracter('9')} className="btn btn-numero">9</button>
+                <button onClick={() => agregarCaracter('/')} className="btn btn-tecla" >÷</button>
               </div>
-              <div className='col-12 d-flex justify-content-between'>
-                <button onClick={() => agregarNum(1)} className="btn btn-numero">1</button>
-                <button onClick={() => agregarNum(2)} className="btn btn-numero">2</button>
-                <button onClick={() => agregarNum(3)} className="btn btn-numero">3</button>
-                <button onClick={() => agregarNum('-')} className="btn btn-tecla" >–</button>
+              <div className='col-md-12 d-flex justify-content-between mb-1'>
+                <button onClick={() => agregarCaracter('4')} className="btn btn-numero">4</button>
+                <button onClick={() => agregarCaracter('5')} className="btn btn-numero">5</button>
+                <button onClick={() => agregarCaracter('6')} className="btn btn-numero">6</button>
+                <button onClick={() => agregarCaracter('*')} className="btn btn-tecla" >×</button>
               </div>
-              <div className='col-12 d-flex justify-content-between'>
-                <button onClick={() => agregarNum(0)} className="btn btn-numero">0</button>
-                <button onClick={() => limpiar()} className="btn btn-tecla" >C</button>
-                <button onClick={() => calculo()} className="btn btn-tecla" >=</button>
-                <button onClick={() => agregarNum('+')} className="btn btn-tecla" >+</button>
+              <div className='col-md-12 d-flex justify-content-between mb-1'>
+                <button onClick={() => agregarCaracter('1')} className="btn btn-numero">1</button>
+                <button onClick={() => agregarCaracter('2')} className="btn btn-numero">2</button>
+                <button onClick={() => agregarCaracter('3')} className="btn btn-numero">3</button>
+                <button onClick={() => agregarCaracter('-')} className="btn btn-tecla" >–</button>
+              </div>
+              <div className='col-md-12 d-flex justify-content-between'>
+                <button onClick={() => agregarCaracter('0')} className="btn btn-numero">0</button>
+                <button onClick={() => agregarCaracter('.')} className="btn btn-tecla" > . </button>
+                <button onClick={() => calcular()} style={{ width: '46%' }} className="btn btn-tecla" >=</button>
               </div>
             </div>
           </div>
