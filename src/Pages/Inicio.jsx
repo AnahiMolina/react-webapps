20c8ce28bb94f6332670a18981a5b14a0532d073
AnahiMolina <anahi_molina11@outlook.com>
2024-05-30 15:25:22 -0600

    Cambio de diseno en la pagina de inicio

--- src/Pages/Inicio.jsx
+++ src/Pages/Inicio.jsx
@@ -1,32 +1,68 @@
 import React from 'react'
-import Contenedor from '../Layouts/Contenedor'
 import Tarjetas from '../components/Tarjetas'
+import Navbar from '../components/Navbar'
+import Carousel from '../components/Carousel'
 
 export default function Inicio() {
   return (
     <>
-      <Contenedor titulo='' icono='' clase='inicio'>
-        <div className='text-center titulos pt-0'>
-          <h1>WebApps</h1>
-          <h1>
-            <i className='bi bi-airplane' />
-          </h1>
+      <div className="inicio">
+        <Navbar />
+        <div className="container-fluid">
+
+          <div className="container pt-4 pb-4">
+            <div className="p-5 bg-primary text-white rounded">
+              <div className="row">
+                <div className="col-md-8">
+                  <h1 className='display-5 fw-bold'>
+                    <i className='bi bi-airplane' /> &nbsp;&nbsp;WebApps
+                  </h1>
+                  <p>
+                    ¡Bienvenido a nuestro repositorio de webapps desarrolladas con React!
+                  </p>
+                </div>
+                <div className="col-md-4 mt-1 mb-2">
+                  <Carousel
+                    link1="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Vitejs-logo.svg/1039px-Vitejs-logo.svg.png"
+                    link2="https://icons.getbootstrap.com/assets/img/icons-hero.png"
+                    link3="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png"
+                  />
+                </div>
+                <hr />
+              </div>
+              <p>
+                Aquí encontrarás una colección de aplicaciones web  construidas utilizando la biblioteca de JavaScript, React. Desde aplicaciones simples hasta proyectos más complejos, hemos reunido una variedad de ejemplos para mostrar el potencial y la versatilidad de Vite React en el desarrollo web moderno.
+              </p>
+            </div>
+          </div>
+
+          <div className="container mt-1">
+            <div className="d-flex justify-content-center m-4">
+              <h1>Aplicaciones</h1>
+            </div>
+            <div className="row pb-5">
+              <div className="col-md-4">
+                <div className="d-flex justify-content-evenly">
+                  <Tarjetas icono='bi bi-palette' link='/colores' nombre='Colores' />
+                  <Tarjetas icono='bi bi-pen' link='/caracteres' nombre='Caracteres' />
+                </div>
+              </div>
+              <div className="col-md-4">
+                <div className="d-flex justify-content-evenly">
+                  <Tarjetas icono='bi bi-stopwatch' link='/reloj' nombre='Reloj' />
+                  <Tarjetas icono='bi bi-calculator' link='/calculadora' nombre='Calculadora' />
+                </div>
+              </div>
+              <div className="col-md-4">
+                <div className="d-flex justify-content-evenly">
+                  <Tarjetas icono='bi bi-cloud-sun' link='/clima' nombre='Clima' />
+                  <Tarjetas icono='bi bi-check2-circle' link='/tareas' nombre='Tareas' />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="row">
-          <div className="col-md-3 mb-5">
-            <Tarjetas link='/colores' nombre='Colores' />
-          </div>
-          <div className="col-md-3 mb-5">
-            <Tarjetas link='/caracteres' nombre='Caracteres' />
-          </div>
-          <div className="col-md-3 mb-5">
-            <Tarjetas link='/reloj' nombre='Reloj' />
-          </div>
-          <div className="col-md-3 mb-2">
-            <Tarjetas link='/calculadora' nombre='Calculadora' />
-          </div>
-        </div>
-      </Contenedor>
+      </div>
     </>
   )
 }
--- src/components/Tarjetas.jsx
+++ src/components/Tarjetas.jsx
@@ -4,11 +4,17 @@ import { Link } from 'react-router-dom'
 export default function Tarjetas(props) {
   return (
     <>
-      <div className="card">
+      <div className="card p-1 m-3">
         <Link to={props.link}>
           <div className="card-body">
-            <div className='center text-center'>
-              {props.nombre}
+            <div className="d-flex justify-content-center">
+              &nbsp;&nbsp;{props.nombre}
+            </div>
+            <hr />
+            <div className="d-flex justify-content-center">
+              <h1>
+                <i className={props.icono} />
+              </h1>
             </div>
           </div>
         </Link>
